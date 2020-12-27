2231efc4b5eab7e0fd557597b90de639bfcb6385
wendy03 <wendysu03@gmail.com>
2020-12-27 14:12:40 +0800

    新增: Footer 區塊

---
+++ src/components/Footer.vue
@@ -0,0 +1,70 @@
+<template>
+  <footer class="footer mt-auto bg-white position-relative"
+          style="z-index: 101">
+    <div class="container pt-md-14 pb-4">
+      <div class="row justify-content-between mb-0 mb-md-14">
+        <div class="d-none d-md-flex flex-md-column">
+          <h3><a class="navbar-brand logo"
+               href="index.html">PETPET</a></h3>
+          <ul class="list-unstyled d-none d-md-block">
+            <li><a href="#"
+                 class="hover-underline">關於我們</a></li>
+            <li><a href="#">幫助中心</a></li>
+            <li><a href="#">使用條款</a></li>
+            <li><a href="#">隱私政策</a></li>
+          </ul>
+        </div>
+        <div class="d-none d-md-flex flex-md-column">
+          <h4 class="h5 text-primary font-weight-bold mb-4 py-2">寵物服務</h4>
+          <ul class="list-unstyled">
+            <li><a href="#">寵物寄宿</a></li>
+            <li><a href="#">寵物到府保姆</a></li>
+            <li><a href="#">寵物美容洗澡</a></li>
+            <li><a href="#">寵物行為訓練</a></li>
+          </ul>
+        </div>
+        <div class="d-none d-md-flex flex-md-column">
+          <h4 class="h5 text-primary font-weight-bold py-2 mb-4">加入我們</h4>
+          <ul class="list-unstyled">
+            <li><a href="#">成為寵物保姆</a></li>
+            <li><a href="#">加入寵物計程車</a></li>
+            <li><a href="#">專屬 APP 下載</a></li>
+            <li><a href="#">相關合約條款</a></li>
+          </ul>
+        </div>
+        <div class="d-none d-md-flex flex-md-column">
+          <h4 class="h5 text-primary font-weight-bold py-2 mb-4">聯繫我們</h4>
+          <ul class="list-unstyled">
+            <li>
+              <a href="mailto:petpet@petmail.com">petpet@petmail.com</a>
+            </li>
+          </ul>
+        </div>
+        <div class="d-flex flex-column d-md-none px-3">
+          <h3><a class="navbar-brand logo"
+               href="index.html">PETPET</a></h3>
+          <h6 class="font-xs text-primary">
+            Copyright © 2020 PetPet. All rights reserved.
+          </h6>
+        </div>
+      </div>
+      <div class="d-none d-md-flex mt-0 mt-mb-14">
+        <h5 class="mr-auto">
+          <span class="material-icons text-primary mr-2"> local_phone </span>
+          <a href="tel+07-123-4567">07-123-4567</a>
+        </h5>
+        <h6 class="text-primary font-weight-normal font-size-sm">
+          Copyright © 2020 PetPet. All rights reserved.
+        </h6>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
