a1fbd9415fd6d2c9885cfac5c0b35b8f07de2132
wendy03 <wendysu03@gmail.com>
2020-12-27 16:30:42 +0800

    修正: Footer 連結

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,32 +4,61 @@
     <div class="container pt-md-14 pb-4">
       <div class="row justify-content-between mb-0 mb-md-14">
         <div class="d-none d-md-flex flex-md-column">
-          <h3><a class="navbar-brand logo"
-               href="index.html">PETPET</a></h3>
+          <h3>
+            <router-link to="/"
+                         class="navbar-brand logo">PETPET
+            </router-link>
+          </h3>
           <ul class="list-unstyled d-none d-md-block">
-            <li><a href="#"
-                 class="hover-underline">關於我們</a></li>
-            <li><a href="#">幫助中心</a></li>
-            <li><a href="#">使用條款</a></li>
-            <li><a href="#">隱私政策</a></li>
+            <li>
+              <router-link to="/"
+                           class="hover-underline">關於我們
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/">幫助中心
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/">使用條款</router-link>
+            </li>
+            <li>
+              <router-link to="/">隱私政策</router-link>
+            </li>
           </ul>
         </div>
         <div class="d-none d-md-flex flex-md-column">
           <h4 class="h5 text-primary font-weight-bold mb-4 py-2">寵物服務</h4>
           <ul class="list-unstyled">
-            <li><a href="#">寵物寄宿</a></li>
-            <li><a href="#">寵物到府保姆</a></li>
-            <li><a href="#">寵物美容洗澡</a></li>
-            <li><a href="#">寵物行為訓練</a></li>
+            <li>
+              <router-link to="/">寵物寄宿</router-link>
+            </li>
+            <li>
+              <router-link to="/">寵物到府保姆</router-link>
+            </li>
+            <li>
+              <router-link to="/">寵物美容洗澡</router-link>
+            </li>
+            <li>
+              <router-link to="/">寵物行為訓練</router-link>
+            </li>
           </ul>
         </div>
         <div class="d-none d-md-flex flex-md-column">
           <h4 class="h5 text-primary font-weight-bold py-2 mb-4">加入我們</h4>
           <ul class="list-unstyled">
-            <li><a href="#">成為寵物保姆</a></li>
-            <li><a href="#">加入寵物計程車</a></li>
-            <li><a href="#">專屬 APP 下載</a></li>
-            <li><a href="#">相關合約條款</a></li>
+            <li>
+              <router-link to="/">成為寵物保姆</router-link>
+            </li>
+            <li>
+              <router-link to="/">加入寵物計程車</router-link>
+            </li>
+            <li>
+              <router-link to="/">專屬 APP 下載</router-link>
+            </li>
+            <li>
+              <router-link to="/">相關合約條款</router-link>
+            </li>
           </ul>
         </div>
         <div class="d-none d-md-flex flex-md-column">
@@ -41,8 +70,10 @@
           </ul>
         </div>
         <div class="d-flex flex-column d-md-none px-3">
-          <h3><a class="navbar-brand logo"
-               href="index.html">PETPET</a></h3>
+          <h3>
+            <router-link to="/"
+                         class="navbar-brand logo">PETPET</router-link>
+          </h3>
           <h6 class="font-xs text-primary">
             Copyright © 2020 PetPet. All rights reserved.
           </h6>
